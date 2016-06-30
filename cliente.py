3c02c082217828f093ad9cf1daba07c977ac784e
Vinnie <vdreifke@inf.ufsm.br>
2016-06-30 18:10:42 -0300

    COMPLETO, CERTINHO PESSOOAALLLLL

--- cliente.py
+++ cliente.py
@@ -6,7 +6,7 @@ from random import choice
 class Cliente:
 
 	HOST = ''
-	PORT = 5554
+	PORT = 5555
 
 
 	def cria_cliente(self):
@@ -22,41 +22,55 @@ class Cliente:
 			msg = input()
 			texto = msg
 			cont = 0
+			conterro = []
+			cont3=0
 			frame = ""
 			salvo = []
+			ERRO=False
 			while len(texto)>0:
-				#listframes = texto[:4]
-
-				frame = texto[:1]
-				texto = texto[1:]
-				ruido = choice([0,1])
-				salvo.append(frame)
-				if ruido == 1:
-					print("RUIDO em frame: ", cont)
-					pos=choice([0,len(frame)-1])
-					if frame[pos]=='1':
-						frame = frame[:pos-1] + "0" + frame[pos:]
+				listframes = texto[:8]
+				texto = texto[8:]
+				cont3=cont
+				for frame in listframes:
+					salvo.append(frame)
+					ruido = choice([0,1])
+					if ruido == 1:
+						print("RUIDO em frame: ", cont)
+						pos=choice([0,len(frame)-1])
+						if frame[pos]=='1':
+							frame = frame[:pos-1] + "0" + frame[pos:]
+						else:
+							frame = frame[:pos-1] + "1" + frame[pos:]
+					#envia check(frame)
+					bits = check(frame)
+					bits = bits.encode('utf8')
+					self.cliente.send(bits)
+					self.cliente.settimeout(5)
+					print("Frame:", cont, "Enviado")
+					confir = self.cliente.recv(2048)
+					confir = confir.decode('utf8')
+					if confir is '0':
+						cont += 1
 					else:
-						frame = frame[:pos-1] + "1" + frame[pos:]
-				#envia check(frame)
-				bits = check(frame)
-				bits = bits.encode('utf8')
-				self.cliente.send(bits)
-				self.cliente.settimeout(5)
-				confir = self.cliente.recv(2048)
-				confir = confir.decode('utf8')
-				if confir is not '0':
-					cont += 1
-				else:
-					for x in salvo:
-						self.cliente.send(bits)
-						self.cliente.settimeout(5)
-						confir = self.cliente.recv(2048)
-						confir = confir.decode('utf8')
-						if confir is '0':
-							print("ACK, Frame reenviada com sucesso")
-					salvo = []
-
+						conterro.append(cont)
+						cont += 1
+						ERRO=True
+				if ERRO:
+					cont2=conterro[0]
+					for frame in salvo:
+						if cont3 >= cont2:
+							#envia check(frame)
+							bits = check(frame)
+							bits = bits.encode('utf8')
+							self.cliente.send(bits)
+							self.cliente.settimeout(5)
+							print("Frame:", cont3, "reenviado")
+							confir = self.cliente.recv(2048)
+							confir = confir.decode('utf8')
+						cont3 += 1
+					salvo=[]
+					conterro=[]
+					ERRO=False
 		self.cliente.close()
 
 cli = Cliente()
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from checksum import *
 class Server:
 
 	HOST = ''
-	PORT = 5554
+	PORT = 5555
 
 	def cria_server(self):
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,18 +16,16 @@ class Server:
 			print('Esperando conexao')
 			self.conexao, self.cliente = self.server.accept()
 			print('Conectado por ', self.cliente)
-			cont = 1
 			while True:
 				msg = self.conexao.recv(2048)
 				if correcao(msg):
-					cont += 1
 					print("Frame verificado com sucesso")
 					msg = '0'
 					#msg = msg.decode('utf8')
 					self.conexao.send(str.encode(msg))
 				else:
 					print("Frame: com erro")
-					msg = str(cont)
+					msg = "1"
 					#msg = msg.decode('utf8')
 					self.conexao.send(str.encode(msg))
 
